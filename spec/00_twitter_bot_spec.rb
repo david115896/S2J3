7986f83f1844607500b914e0b558204c6ef85797
david115896 <dtaccori@gmail.com>
2019-10-09 11:40:27 +0200

    create repo files

---
+++ spec/00_twitter_bot_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/twitter_bot' #fichier .rb
+
+describe "name_method" do
+  it "explain_what_should_do_the_method5" do
+    expect(methode_name(variables_to_send)).to eq(result_expected)
+    expect(methode_name(variables_to_send)).to eq(result_expected)
+  end
+end
